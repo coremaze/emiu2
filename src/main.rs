678172ea0e40be2f5db67cadd0f835beb3f780aa
ChrisMiuchiz <chris000005@gmail.com>
2023-01-01 19:19:01 -0500

    Add cli arguments

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,44 @@ mod display;
 mod gpio;
 pub mod memory;
 mod miuchiz;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Miuchiz OTP image
+    otp_file: String,
+
+    /// Miuchiz flash image
+    flash_file: String,
+
+    /// Pixel scale
+    #[arg(short, long, default_value_t = 3)]
+    scale: usize,
+}
 
 fn main() {
-    let screen = display::MiniFbScreen::open("emiu2", 98, 67, 3);
+    let args = Args::parse();
 
-    let otp_data = std::fs::read("OTP.dat").unwrap();
-    let flash_data = std::fs::read("Flash.dat").unwrap();
+    let otp_data = match std::fs::read(args.otp_file) {
+        Ok(data) => data,
+        Err(why) => {
+            eprintln!("Could not read OTP file: {why}");
+            return;
+        }
+    };
+
+    let flash_data = match std::fs::read(args.flash_file) {
+        Ok(data) => data,
+        Err(why) => {
+            eprintln!("Could not read OTP file: {why}");
+            return;
+        }
+    };
+
+    let scale = args.scale;
+
+    let screen = display::MiniFbScreen::open("emiu2", 98, 67, scale);
+
     let mut handheld = miuchiz::Handheld::new(&otp_data, &flash_data, &screen, &screen).unwrap();
     // std::thread::sleep(std::time::Duration::from_secs(3));
 
@@ -20,7 +52,6 @@ fn main() {
         let cycles_required_so_far =
             (microseconds * handheld.mcu.core.cycles_per_second() as u128) / 1000000000;
 
-        // println!("need to do {cycles_to_do:?} cycles");
         while (handheld.mcu.core.cycles as u128) < cycles_required_so_far {
             // let pc = handheld.mcu.core.registers.pc;
             // let inst = handheld.mcu.core.decode_next_instruction();
@@ -30,19 +61,5 @@ fn main() {
 
         std::thread::sleep(std::time::Duration::from_nanos(1));
     }
-
-    // for _count in 0u64..800_000_000 {
-    //     // let inst = handheld.mcu.core.decode_next_instruction();
-    //     let pc = handheld.mcu.core.registers.pc;
-    //     // println!("{pc:04X}: {}", inst.instruction.to_string());
-    //     handheld.mcu.step();
-    //     // println!("{}", handheld.mcu.core.registers.to_string());
-    //     // if pc == 0x5972 {
-    //     //     break;
-    //     // }
-    //     if !screen.is_open() {
-    //         break;
-    //     }
-    // }
     println!("{} cycles", handheld.mcu.core.cycles);
 }
